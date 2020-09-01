88741ee47d671e27e929e366cc12507fa9561fe6
Daniel Wang <hello@danielwang.dev>
2020-09-01 15:33:48 -0400

    feat: share link

--- app/main.py
+++ app/main.py
@@ -64,15 +64,10 @@ def faq():
 
 
 @app.route("/edit", methods=["GET", "POST"])
-@limiter.limit("4/hour", methods=["POST"])
+@limiter.limit("8/hour", methods=["POST"])
 def edit():
     if not current_user.is_authenticated:
         return redirect(url_for("home"))
-
-    if current_user.district == 0:
-        flash('Instructions: Enter teacher names corresponding to each period in your list schedule. If a class has more than one teacher, enter the name of the first/primary teacher. Do not enter lab periods. For study hall periods enter either "North - Study Hall" or "South - Study Hall".', "info")
-    else:
-        flash('Instructions: Enter teacher names corresponding to each period in your schedule. If a class has more than one teacher, enter the name of the first/primary teacher. Do not enter lab periods. For lunch periods enter "Lunch".', "info")
 
     if request.method == "POST":
         for period, teacher in request.form.to_dict().items():
@@ -89,4 +84,31 @@ def edit():
         }
         requests.post(url=app.config["DISCORD_WEBHOOK_URL"], data=PARAMS)
         return redirect(url_for("view"))
+
+    if current_user.district == 0:
+        flash('Instructions: Enter teacher names corresponding to each period in your list schedule. If a class has more than one teacher, enter the name of the first/primary teacher. Do not enter lab periods. For study hall periods enter either "North - Study Hall" or "South - Study Hall". If your teacher isn\'t in the list, you can type it manually.', "info")
+    else:
+        flash('Instructions: Enter teacher names corresponding to each period in your schedule. If a class has more than one teacher, enter the name of the first/primary teacher. Do not enter lab periods. For lunch periods enter "Lunch".', "info")
+
     return render_template("edit.html")
+
+@app.route("/share/<provider_user_id>")
+def share(provider_user_id):
+    oauth = OAuth.query.filter_by(provider_user_id=provider_user_id).first_or_404()
+
+    return render_template(
+        "share.html", name=oauth.user.name, schedule=oauth.user.schedule.get()
+    )
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html", error="404 - Page not found."), 404
+
+@app.errorhandler(429)
+def too_many_requests(e):
+    return render_template("error.html", error="429 - You have been rate limited."), 429
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("error.html", error="500 - Server Error. Please try again."), 429
+
